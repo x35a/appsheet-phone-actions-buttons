5ca0729d74d6f6b7c03262896581e32592117e8d
xj <8172473+x35a@users.noreply.github.com>
2022-08-15 01:45:38 +0300

    add build-tracking-link

---
+++ src/build-tracking-link.js
@@ -0,0 +1,22 @@
+import { trackingNumber } from "../src/read-url-parameters";
+
+const novaposhtaTrackingLink = `https://novaposhta.ua/tracking/?cargo_number=${trackingNumber}`;
+const ukrposhtaTrackingLink = `https://track.ukrposhta.ua/tracking_UA.html?barcode=${trackingNumber}`;
+const justinTrackingLink = `https://justin.ua/tracking?number=${trackingNumber}`;
+
+export let trackingLink;
+
+switch (trackingNumber.slice(0, 2)) {
+  case "20":
+  case "59":
+    trackingLink = novaposhtaTrackingLink;
+    break;
+  case "05":
+    trackingLink = ukrposhtaTrackingLink;
+    break;
+  case "42":
+    trackingLink = justinTrackingLink;
+    break;
+  default:
+    trackingLink = "";
+}
