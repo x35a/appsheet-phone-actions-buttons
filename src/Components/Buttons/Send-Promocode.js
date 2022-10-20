5298e441c10bfad30b48b4ce29856732610513b6
xj <8172473+x35a@users.noreply.github.com>
2022-10-20 17:16:42 +0300

    upd SendPromocode

--- src/Components/Buttons/Send-Promocode.js
+++ src/Components/Buttons/Send-Promocode.js
@@ -1,22 +1,45 @@
 import React from "react";
-import { SimpleButton } from "./button-templates/Simple-Button";
+import { ButtonWithOptions } from "./button-templates/Button-with-options";
 import { orderId, phoneNumber } from "../../read-url-parameters";
 import { getHref } from "../../get-href";
 
-export const SendPromocode = (props) => {
-  const href = getHref(phoneNumber).viber;
-  const iconClass = "las la-percent";
-  const buttonText = "Send Promocode";
-  const message = `Пропонуємо промокод на знижку 3%
+export const SendPromocode = () => {
+  const props = {
+    href: getHref(phoneNumber).viber,
+    iconClass: "las la-percent",
+    options: [
+      {
+        checked: true,
+        inputText: "smartfood promo with expire date",
+        message: `Пропонуємо промокод на знижку 3%
 Ваш промокод: ${orderId}
-Скористатися знижкою можна на сайті https://smartfood.org.ua/`;
+Скористатися знижкою можна на сайті https://smartfood.org.ua/
+Термід дії промокоду складає один місяць.`
+      },
+      {
+        checked: false,
+        inputText: "smartfood promo no limits",
+        message: `Пропонуємо промокод на знижку 3%
+Ваш промокод: ${orderId}
+Скористатися знижкою можна на сайті https://smartfood.org.ua/`
+      },
+      {
+        checked: false,
+        inputText: "homemarket promo with expire date",
+        message: `Пропонуємо промокод на знижку 3%
+Ваш промокод: ${orderId}
+Скористатися знижкою можна на сайті https://homemarket.org.ua/
+Термід дії промокоду складає один місяць.`
+      },
+      {
+        checked: false,
+        inputText: "homemarket promo no limit",
+        message: `Пропонуємо промокод на знижку 3%
+Ваш промокод: ${orderId}
+Скористатися знижкою можна на сайті https://homemarket.org.ua/`
+      }
+    ]
+  };
 
-  return (
-    <SimpleButton
-      href={href}
-      iconClass={iconClass}
-      buttonText={buttonText}
-      message={message}
-    />
-  );
+  return <ButtonWithOptions {...props} />;
 };
